3a2607f79e393b9039a51b499482199cad724dd4
ben411916 <ben.allington1@gmail.com>
2025-05-28 15:49:19 +0100

    Fix remaining TypeScript type inference issues with explicit annotations

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "colyseus-server",
+      script: "./lib/index.js",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+}
